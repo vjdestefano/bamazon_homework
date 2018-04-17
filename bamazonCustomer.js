aede64183c7a2ae2c63ddd107e4f057dc8f5753f
Vinny DeStefano <vjdestefano500@gmail.com>
2018-04-17 17:48:07 -0400

    this is working homework part 1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -33,8 +33,101 @@ function start(){
 
 function browseItems(){
   connection.query("SELECT * FROM products;",function(err, res){
-    console.log(res);
+    //console.log(res);
+    res.forEach(function(item, index) {
+      console.log(item.id + ": " + item.item_name + "- $" + item.price + "|Quanity: " + item.stock_quantity);
+    });
+    return buyItem();
   })
 }
+
+function buyItem(){
+  inquirer.prompt([
+    {
+      type:"input",
+      message:"Please tell is the ID of the Item you want to buy. ex(4)",
+      name:"customerPurchase",
+      validate: function(val){
+        if(!isNaN(val)){
+          return true;
+        } else {
+          return "Please insert a another number";
+        }
+      }
+    }, {
+      type:"input",
+      message:"How many would you like to buy?",
+      name:"customerAmount",
+      validate: function(val){
+        if(!isNaN(val)){
+          return true;
+        } else {
+          return "Please insert a another number";
+        }
+      }
+    },
+  ]).then(function(inquirerResponse){
+    console.log("this is your item: " + inquirerResponse.customerPurchase + " " + typeof inquirerResponse.customerPurchase);
+    return checkItem(parseInt(inquirerResponse.customerPurchase), parseInt(inquirerResponse.customerAmount));
+  })
+}
+
+function checkItem(numID, numOfItems){
+  console.log(numID + " and " + numOfItems);
+  connection.query("SELECT * FROM products WHERE id=?",[numID],function(err, res){
+
+    var selectedItem = res[0].item_name;
+    var amountLeft = res[0].stock_quantity - numOfItems;
+
+    res.forEach(function(item, index) {
+      console.log(item.id + ": " + item.item_name + "- $" + item.price + "|Quanity: " + item.stock_quantity);
+    });
+    console.log(res[0].stock_quantity);
+    console.log("this is the amount left: " + amountLeft);
+    //check at see if the number of items are able to be purchased
+
+    if(res[0].stock_quantity< numOfItems){
+      console.log("this item doesn't have that many units availble");
+      return buyItem();
+    }
+    if(res[0].stock_quantity === numOfItems){
+      console.log("this item can be purchased but you will wipe out all of the stock");
+      return checkout(numID, selectedItem, numOfItems, amountLeft);
+    }
+    if(res[0].stock_quantity > numOfItems){
+      console.log("we have more than enough product for you");
+      return checkout(numID, selectedItem, numOfItems, amountLeft);
+    }
+
+    });
+  }
+
+function checkout(numID, selectedItem, numOfItems, amountLeft){
+    inquirer.prompt([
+       {
+      type: "list",
+      message: "are you sure you want to purchase?",
+      choices:["yes", "no"],
+      name: "confirm",
+    },
+    ]).then(function(inquirerResponse){
+      if(inquirerResponse.confirm === "yes"){
+        console.log(inquirerResponse.confirm);
+        connection.query("UPDATE products SET stock_quantity=? WHERE id=?",[
+          amountLeft,
+          numID
+        ],
+           function(err, res){
+          console.log(numID + ": " + selectedItem + " |Quantity Left: " + amountLeft);
+          return browseItems();
+        })
+
+      } else{
+        return browseItems();
+      }
+
+    })
+  }
+
 
 start();
